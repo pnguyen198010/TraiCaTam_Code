b03f3b9b3a6428b9653d613de523ae8c21d8151d
nguyendaiphuc151006 <nguyendaiphuc151006@gmail.com>
2023-04-06 12:15:14 +0700

    Something changed

--- Code_Slave_Turbidity/src/main.cpp
+++ Code_Slave_Turbidity/src/main.cpp
@@ -1,51 +1,19 @@
-// gui
+
+
 #include <Arduino.h>
-#include <LoRa.h>
-#include <SoftwareSerial.h>
-#define tx 5
-#define rx 6
-#define nut 12 // D2
-#define led 2 // D10 
-#define M1 8
-#define M2 7
-const char *ketqua1;
-SoftwareSerial tai(tx,rx); 
-/*****************************************/
 
-void setup() {
-Serial.begin(9600);  
-tai.begin(9600); 
-pinMode(nut,INPUT_PULLUP); 
-pinMode(led,OUTPUT);
-pinMode(M1, OUTPUT);
-pinMode(M2, OUTPUT);
+#include "log.h"
+#include "lora_service.h"
 
 
-digitalWrite(led, LOW);
-digitalWrite(M1, LOW);
-digitalWrite(M2, LOW);
+void setup() 
+{
+  Log_init();
+  Lora_init();
 }
 
-/*****************************************/
 
-void loop() {
-
- int a = digitalRead(nut);
-   
-    if(a==LOW){
-          ketqua1="1";         
-          tai.write(ketqua1);
-          digitalWrite(led, HIGH);
-          Serial.println(ketqua1);
-          delay(10);
-              }
-              
-    else {
-          ketqua1="2";
-          tai.write(ketqua1);
-          digitalWrite(led, LOW);
-          Serial.println(ketqua1);
-          delay(100);
-         }
-         
+void loop() 
+{
+  Lora_upd_turbidity();
 }
